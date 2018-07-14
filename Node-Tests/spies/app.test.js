12351f2533e8eaa83b742c8c314840f7fe228d80
mhd3v <mamir3012@gmail.com>
2018-07-14 18:52:01 +0500

    Testing using spies

---
+++ Node-Tests/spies/app.test.js
@@ -0,0 +1,34 @@
+const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
+
+
+describe('App', () => {
+
+    var db = {
+        saveUser: expect.createSpy()
+    };
+
+    app.__set__('db', db);
+
+    it('should call spy correctly', () => {
+
+        var spy = expect.createSpy();
+        spy();
+        expect(spy).toHaveBeenCalled();
+
+    });
+
+    it('should call saveUser with user object', () => {
+
+        var email = "someemail@email.com";
+        var password = "123";
+
+        app.handleSignup(email, password);
+
+        expect(db.saveUser).toHaveBeenCalledWith({email, password});
+
+    });
+
+});
