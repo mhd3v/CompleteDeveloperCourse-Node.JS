80a91dc7a0b3a07801ae1a919284248d311c475a
mhd3v <mamir3012@gmail.com>
2018-07-16 00:52:21 +0500

    Add delete script

--- Node-Todo-API/playground/mongodb-connect.js
+++ Node-Todo-API/playground/mongodb-connect.js
@@ -1,19 +1,4 @@
-//==========================================================================================
-//destructuring (making varaibles from an object's property (ES6 syntax))
-
-// var myObj = {name: 'mahad', age: 12};
-// var {name} = myObj;
-// console.log(name);
-
-//=================
-
-//const MongoClient = require('mongodb').MongoClient; 
-const {MongoClient, ObjectID} = require('mongodb'); //pull out MongoClient and ObjectID from require('mongodb')
-
-var obj = new ObjectID(); //this will generate a unique mongo obj id
-
-console.log(obj);
-
+const MongoClient = require('mongodb').MongoClient; 
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser:  true} , (err, client) => { //mongodb://localhost:27017/TodoApp will create TodoApp database if it doesnt exist
 
@@ -62,49 +47,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser:  true
         
     // });
 
-    //==========================================================================================
-
-    //client.collection('Todos').find(); returns a mongodb cursor
-    //client.collection('Todos').find().toArray() toArray returns a promise
-
-    // db.collection('Todos').find({ // specify query as a set of key value pairs to find()
-    //     _id: new ObjectID('5b4b650f06cb0d2010e177c0')
-    
-    // }).toArray().then((res) => { 
-
-    //     console.log('Todo List: ');
-    //     console.log(JSON.stringify(res, undefined, 2));
-
-    // }, (err) => {
-    //     console.log('Unable to fetch todos', err)
-    // })
-
-    //==========================================================================================
-
-    // db.collection('Todos').find({ // specify query as a set of key value pairs to find()
-        
-    //     //specify query here, as key-value pairs
-    //     //_id: new ObjectID('5b4b650f06cb0d2010e177c0')
-    
-    // }).count().then((count) => { 
-
-    //     console.log(`Todo count: ${count} `);
-
-    // }, (err) => {
-    //     console.log('Unable to fetch todos', err)
-    // })
-
-    //==========================================================================================
-
-
-    db.collection('Users').find({name: 'Mahad'}).toArray().then((documents) => {
-
-        console.log(JSON.stringify(documents, undefined, 2));
-
-    }, (err) => {
-        console.log('Unable to fetch to-dos', err);
-    });
-
-    //client.close();
+    client.close();
 
 });
