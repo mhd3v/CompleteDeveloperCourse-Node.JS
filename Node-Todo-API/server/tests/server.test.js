67effc5446273a4d585e67077d7980ce924a374f
mhd3v <mamir3012@gmail.com>
2018-07-19 16:15:08 +0500

    Testting for GET /todos/:id route

--- Node-Todo-API/server/tests/server.test.js
+++ Node-Todo-API/server/tests/server.test.js
@@ -1,11 +1,12 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 
-var dummyTodos = [{text: 'do something'}, {text: 'second test text'}];
+var dummyTodos = [{text: 'do something', _id: new ObjectID()}, {text: 'second test text', _id: new ObjectID()}];
 
 
 //beforeEach lets us run some code before each test
@@ -77,4 +78,30 @@ describe('GET /todo', () =>{
 
     });
 
+});
+
+describe('GET /todo/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app) 
+        .get(`/todos/${dummyTodos[0]._id.toHexString()}`) //cant pass object ID driectly to the url 
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(dummyTodos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        request(app)
+        .get(`/todos/${(new ObjectID()).toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', () => {
+        request(app)
+        .get('todos/123')
+        .expect(404)
+        .end(done);
+    });
 });
